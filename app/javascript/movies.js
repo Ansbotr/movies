663e3143a69511726da4905ddb650fc977923600
Ansbotr <anastasiia.bb.ua@gmail.com>
2022-08-08 09:58:36 +0300

    mode modal ui

--- app/javascript/movies.js
+++ app/javascript/movies.js
@@ -1,24 +1,24 @@
-
-
-$(document).ready(function() {
-
-// ==== backToTop  ====
-    var $backToTop = $(".back-to-top");
-    // $backToTop.hide();
-  //Scroll event
-  $(window).scroll(function(){
-    var scrolled = $(window).scrollTop();
-    // console.log(scrolled);
-    if (scrolled > 25) {
-        $backToTop.addClass("active");
-      } else {
-        $backToTop.removeClass("active");
-      }
-
-  });
-  $backToTop.on('click', function(e) {
-    $("html, body").animate({scrollTop: 0}, 500);
-  });
-
-//  END JQUERY
-  });
+//
+//
+// $(document).ready(function() {
+//
+// // ==== backToTop  ====
+//     var $backToTop = $(".back-to-top");
+//     // $backToTop.hide();
+//   //Scroll event
+//   $(window).scroll(function(){
+//     var scrolled = $(window).scrollTop();
+//     // console.log(scrolled);
+//     if (scrolled > 25) {
+//         $backToTop.addClass("active");
+//       } else {
+//         $backToTop.removeClass("active");
+//       }
+//
+//   });
+//   $backToTop.on('click', function(e) {
+//     $("html, body").animate({scrollTop: 0}, 500);
+//   });
+//
+// //  END JQUERY
+//   });
